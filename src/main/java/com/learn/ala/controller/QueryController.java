2fe3554974f4105d51e62aad374af1c375905889
Ankit Srivastava <ankit.srivastava@ameri100.com>
2020-10-29 14:33:51 +0530

    resolve cross origin

--- src/main/java/com/learn/ala/controller/QueryController.java
+++ src/main/java/com/learn/ala/controller/QueryController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.learn.ala.model.Questions;
 import com.learn.ala.repository.QuestionRepository;
 import com.learn.ala.service.TopicService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1")
 public class QueryController {
