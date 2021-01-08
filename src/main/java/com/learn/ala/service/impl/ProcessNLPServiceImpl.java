a538030bdc8b805e0ced072ac27a697b717246d6
Ankit Srivastava <ankit.srivastava@ameri100.com>
2021-01-08 09:37:57 +0530

    bot working fine in LINUX as well as in Window

--- src/main/java/com/learn/ala/service/impl/ProcessNLPServiceImpl.java
+++ src/main/java/com/learn/ala/service/impl/ProcessNLPServiceImpl.java
@@ -1,40 +1,20 @@
 package com.learn.ala.service.impl;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.io.InputStream;
 
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.alicebot.ab.History;
 import org.alicebot.ab.MagicBooleans;
 import org.alicebot.ab.MagicStrings;
-import org.alicebot.ab.utils.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
-import org.springframework.web.client.RestTemplate;
 
-import com.learn.ala.model.Questions;
-import com.learn.ala.nlp_application.core.Pipeline;
 import com.learn.ala.repository.QuestionRepository;
 import com.learn.ala.service.ProcessNLPService;
 import com.learn.ala.service.TopicService;
-import com.learn.ala.util.Type;
-
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import io.jsonwebtoken.lang.Collections;
 
 @Service
 public class ProcessNLPServiceImpl implements ProcessNLPService{
@@ -45,7 +25,6 @@ public class ProcessNLPServiceImpl implements ProcessNLPService{
 	@Autowired
 	TopicService topicService;
 
-	public static String URL_RESOURCE = "http://localhost:8080/api/v1/ner";
 	private static final boolean TRACE_MODE = false;
 	static String botName = "super";
 	
@@ -55,62 +34,18 @@ public class ProcessNLPServiceImpl implements ProcessNLPService{
 	Bot bot = new Bot("super", resourcesPath);
 	Chat chatSession = new Chat(bot);
 
-//	@Override
-//	public List<String> processNLP(String input, Type type) {
-//		
-//		StanfordCoreNLP stanfordCoreNLP = Pipeline.getInstance();
-//		  RestTemplate restTemplate = new RestTemplate();
-//		  
-//		  List<Questions> topicToSelect = null;
-//		  List<String> difficultyLevel = new ArrayList<String>();
-//		  
-//		  HttpHeaders headers = new HttpHeaders(); //
-//		  headers.setContentType(MediaType.APPLICATION_JSON); 
-//		  MultiValueMap<String, String> map = new LinkedMultiValueMap<>(); 
-//		  map.add("input", input);
-//		  HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-//
-//		  ResponseEntity<List<String>> res = restTemplate.exchange(URL_RESOURCE + "?type="+type, HttpMethod.POST, request, new ParameterizedTypeReference<List<String>>() { });
-//		 if(!CollectionUtils.isEmpty(res.getBody()))
-//			{
-//				topicToSelect = topicService.getTopics();
-//
-//				for (String top : res.getBody()) {
-//
-//					topicToSelect.stream().filter(topic -> top.equalsIgnoreCase(topic.getTopic()))
-//							.collect(Collectors.toList());
-//					if (!CollectionUtils.isEmpty(topicToSelect)) {
-//						difficultyLevel = topicService.retrieveDifficultyLevelByTopic(topicToSelect.get(0).getTopic());
-//						return difficultyLevel;
-//					}
-//				}
-//		 }
-////		  if(!StringUtils.isEmpty(topicToSelect)) {
-////			  difficultyLevel = topicService.retrieveDifficultyLevelByTopic(topicToSelect.get().getTopic());
-////			  return difficultyLevel;
-////		  }
-//		  
-//		  difficultyLevel.add("We are not suppose to answer this query");
-//		  		
-//		  return difficultyLevel;
-//	}
-
 	@Override
 	public String processBoatRequest(String input) {
 		
 	try {
-//		String resourcesPath = getResourcesPath();
-//		System.out.println("ResourcePath-: "+resourcesPath);
-//		MagicBooleans.trace_mode = TRACE_MODE;
-//		Bot bot = new Bot("super", resourcesPath);
-//		Chat chatSession = new Chat(bot);
 		MagicBooleans.trace_mode = TRACE_MODE;
 		bot.brain.nodeStats();
 		String response = null;
 		
 
 		while(true) {
-			System.out.print("Human : ");
+			
+			System.out.println("Human : "+input);
 			
 			if ((input == null) || (input.length() < 1))
 				input = MagicStrings.null_input;
@@ -139,13 +74,51 @@ public class ProcessNLPServiceImpl implements ProcessNLPService{
 	return null;
 		
 	}
+
+
+	
 	private static String getResourcesPath() {
 		File currDir = new File(".");
-		String path = currDir.getAbsolutePath();
-		path = path.substring(0, path.length() - 2);
+		String path = null;
+		try {
+			path = currDir.getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Path befor lenght deduction--"+path);
+		//path = path.substring(0, path.length() - 2);
 		System.out.println(path);
+		File folderpath = new File(path+ File.separator+"bots");
+		if(folderpath.exists())
+			return path;
 		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
 		return resourcesPath;
 	}
+	
+    /*
+    The resource URL is not working in the JAR
+    If we try to access a file that is inside a JAR,
+    It throws NoSuchFileException (linux), InvalidPathException (Windows)
 
+    Resource URL Sample: file:java-io.jar!/json/file1.json
+ */
+//private File getFileFromResource(String fileName) throws URISyntaxException{
+//
+//    ClassLoader classLoader = getClass().getClassLoader();
+//    URL resource = classLoader.getResource(fileName);
+//    if (resource == null) {
+//        throw new IllegalArgumentException("file not found! " + fileName);
+//    } else {
+//
+//        // failed if files have whitespaces or special characters
+//        //return new File(resource.getFile());
+//
+//        return new File(resource.toURI());
+//    }
+//
+//}
+
+
+	
 }
---
+++ src/main/java/com/learn/ala/util/Timer.java
@@ -0,0 +1,27 @@
+package com.learn.ala.util;
+public class Timer
+{
+  private long startTimeMillis;
+
+  public Timer()
+  {
+    start();
+  }
+  public void start() {
+    this.startTimeMillis = System.currentTimeMillis();
+  }
+  public long elapsedTimeMillis() {
+    return System.currentTimeMillis() - this.startTimeMillis + 1L;
+  }
+  public long elapsedRestartMs() {
+    long ms = System.currentTimeMillis() - this.startTimeMillis + 1L;
+    start();
+    return ms;
+  }
+  public float elapsedTimeSecs() {
+    return (float)elapsedTimeMillis() / 1000.0F;
+  }
+  public float elapsedTimeMins() {
+    return elapsedTimeSecs() / 60.0F;
+  }
+}
